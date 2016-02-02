808b5317b559d0ba9aae50b0d07288668135a875
Daniel McSorley <mcsorley.1@gmail.com>
2016-02-02 10:54:09 -0500

    pull image and create container

--- clusteratorcommander.go
+++ clusteratorcommander.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/docker/engine-api/client"
 	"github.com/docker/engine-api/types"
+	"github.com/docker/engine-api/types/container"
+	"github.com/docker/engine-api/types/strslice"
 	"github.com/docker/go-connections/tlsconfig"
 	"github.com/docker/machine/commands/mcndirs"
 	"github.com/docker/machine/libmachine"
@@ -14,6 +16,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+)
+
+const (
+	CONSUL_CONTAINER_NAME = "clusterator_consul"
 )
 
 func loadHost(api *libmachine.Client, hostname string) *host.Host {
@@ -25,7 +32,7 @@ func loadHost(api *libmachine.Client, hostname string) *host.Host {
 }
 
 func getHostOptions(host *host.Host, swarm bool) (string, *auth.Options) {
-	dockerHost, authOptions, err := check.DefaultConnChecker.Check(host, true)
+	dockerHost, authOptions, err := check.DefaultConnChecker.Check(host, swarm)
 	if err != nil {
 		log.Fatal("Error running connection boilerplate: %s", err)
 	}
@@ -33,53 +40,7 @@ func getHostOptions(host *host.Host, swarm bool) (string, *auth.Options) {
 	return dockerHost, authOptions
 }
 
-func rewriteConfig(api *libmachine.Client, hostname string) {
-	host := loadHost(api, hostname)
-	ip, _ := host.Driver.GetIP()
-
-	swarmopts := host.HostOptions.SwarmOptions
-	swarmopts.IsSwarm = true
-	swarmopts.Master = true
-	swarmopts.Discovery = fmt.Sprintf("consul://%s:8500/barney", ip)
-
-	api.Save(host)
-}
-
-type hostable func(*host.Host)
-
-func forAllHosts(api *libmachine.Client, hostnames []string, applicable hostable) {
-	for _, hostname := range hostnames {
-		host := loadHost(api, hostname)
-		applicable(host)
-	}
-}
-
-func dmIP(api *libmachine.Client, hostnames []string) {
-	forAllHosts(api, hostnames, func(host *host.Host) {
-		ip, _ := host.Driver.GetIP()
-		fmt.Println(host.Name, ip)
-	})
-}
-
-func printJson(api *libmachine.Client, hostname string) {
-	host := loadHost(api, hostname)
-	prettyJSON, err := json.MarshalIndent(host, "", "    ")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(string(prettyJSON))
-}
-
-func dmConfig(api *libmachine.Client, hostname string) {
-	host := loadHost(api, hostname)
-	dockerHost, authOptions := getHostOptions(host, true)
-
-	fmt.Printf("--tlsverify\n--tlscacert=%q\n--tlscert=%q\n--tlskey=%q\n-H=%s\n",
-		authOptions.CaCertPath, authOptions.ClientCertPath, authOptions.ClientKeyPath, dockerHost)
-}
-
-func getClient(dockerHost string, authOptions *auth.Options) *client.Client {
+func createClient(dockerHost string, authOptions *auth.Options) *client.Client {
 	// based on docker/engine-api/client.NewEnvClient
 	options := tlsconfig.Options{
 		CAFile:             authOptions.CaCertPath,
@@ -107,10 +68,63 @@ func getClient(dockerHost string, authOptions *auth.Options) *client.Client {
 	return cli
 }
 
+func rewriteConfig(api *libmachine.Client, hostname string) {
+	host := loadHost(api, hostname)
+	ip, _ := host.Driver.GetIP()
+
+	swarmopts := host.HostOptions.SwarmOptions
+	swarmopts.IsSwarm = true
+	swarmopts.Master = true
+	swarmopts.Discovery = fmt.Sprintf("consul://%s:8500/barney", ip)
+
+	api.Save(host)
+}
+
+type hostable func(*host.Host)
+type indexedhostable func(int, *host.Host)
+
+func forAllHostsIndexed(api *libmachine.Client, hostnames []string, applicable indexedhostable) {
+	for index, hostname := range hostnames {
+		host := loadHost(api, hostname)
+		applicable(index, host)
+	}
+}
+
+func forAllHosts(api *libmachine.Client, hostnames []string, applicable hostable) {
+	forAllHostsIndexed(api, hostnames, func(index int, host *host.Host) {
+		applicable(host)
+	})
+}
+
+func dmIP(api *libmachine.Client, hostnames []string) {
+	forAllHosts(api, hostnames, func(host *host.Host) {
+		ip, _ := host.Driver.GetIP()
+		fmt.Println(host.Name, ip)
+	})
+}
+
+func printJson(api *libmachine.Client, hostname string) {
+	host := loadHost(api, hostname)
+	prettyJSON, err := json.MarshalIndent(host, "", "    ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(prettyJSON))
+}
+
+func dmConfig(api *libmachine.Client, hostname string) {
+	host := loadHost(api, hostname)
+	dockerHost, authOptions := getHostOptions(host, true)
+
+	fmt.Printf("--tlsverify\n--tlscacert=%q\n--tlscert=%q\n--tlskey=%q\n-H=%s\n",
+		authOptions.CaCertPath, authOptions.ClientCertPath, authOptions.ClientKeyPath, dockerHost)
+}
+
 func dPs(api *libmachine.Client, hostname string) {
 	host := loadHost(api, hostname)
 	dockerHost, authOptions := getHostOptions(host, true)
-	cli := getClient(dockerHost, authOptions)
+	cli := createClient(dockerHost, authOptions)
 
 	containers, err := cli.ContainerList(types.ContainerListOptions{All: true})
 	if err != nil {
@@ -128,6 +142,86 @@ func dmStart(api *libmachine.Client, hostnames []string) {
 	})
 }
 
+func startFirstMember(api *libmachine.Client, hostname string, quorum int) (string, error) {
+	host := loadHost(api, hostname)
+	ip, _ := host.Driver.GetIP()
+	dockerHost, authOptions := getHostOptions(host, false)
+	cli := createClient(dockerHost, authOptions)
+
+	pullOptions := types.ImagePullOptions{
+		ImageID: "progrium/consul",
+		Tag: "latest",
+	}
+
+	if resp, err := cli.ImagePull(pullOptions, nil); err != nil {
+		return "", err
+	} else {
+		fmt.Println("Pull response", resp);
+		// TODO: wait for pull to finish
+	}
+
+	containerConfig := &container.Config{
+		Image: "progrium/consul",
+		Cmd: strslice.New("-server", "-bind", ip, "-bootstrap-expect", strconv.Itoa(quorum)),
+	}
+
+	hostConfig := &container.HostConfig{
+		NetworkMode: "host",
+		LogConfig: container.LogConfig{
+			Type: "json-file",
+			Config: map[string]string{
+				"max-size": "10m",
+				"max-file": "5",
+			},
+		},
+		RestartPolicy: container.RestartPolicy{
+			Name: "always",
+		},
+	}
+
+	if resp, err := cli.ContainerCreate(containerConfig, hostConfig, nil, CONSUL_CONTAINER_NAME); err != nil {
+		return "", err
+	} else {
+		fmt.Println("Create Response", resp)
+	}
+
+	// TODO: start the newly created container
+
+	return ip, nil
+}
+
+func clStart(api *libmachine.Client, hostnames []string) {
+	quorum := len(hostnames)/2 + 1
+	ip, err := startFirstMember(api, hostnames[0], quorum)
+	if err != nil {
+		fmt.Println(err);
+		return
+	}
+	//fmt.Println(quorum, CONSUL_CONTAINER_NAME)
+	fmt.Println(ip)
+
+	//forAllHostsIndexed(api, hostnames, func(index int, host *host.Host) {
+		//ip, _ := host.Driver.GetIP()
+		//fmt.Println(index, host.Name, ip)
+		//dockerHost, authOptions := getHostOptions(host, false)
+		//cli := createClient(dockerHost, authOptions)
+
+		//containerConfig := &container.Config{
+			//Image: "progrium/consul",
+			//Cmd: []string{"-server", "-bind", ip, "-bootstrap-expect", quorum}
+		//}
+//CONSUL_QUORUM="-bootstrap-expect $(($MACHINE_COUNT / 2 + 1))"
+//CONSUL_JOINABLE="-join $(docker-machine ip $FIRST):8301"
+// progrium/consul
+// net: host
+// log_opt:
+//max-size: "10m"
+//max-file: "5"
+// command: -server -bind ${MACHINE_IP} ${CONSUL_QUORUM_OR_JOIN}
+//FIRST=${MACHINE_NAMES[0]}
+	//})
+}
+
 func main() {
 	api := libmachine.NewClient(mcndirs.GetBaseDir(), mcndirs.GetMachineCertDir())
 	defer api.Close()
@@ -141,6 +235,7 @@ func main() {
 	case "config": dmConfig(api, os.Args[2])
 	case "ps": dPs(api, os.Args[2])
 	case "start": dmStart(api, os.Args[2:])
+	case "startcluster": clStart(api, os.Args[2:])
 	default: fmt.Println("nope!")
 	}
 
